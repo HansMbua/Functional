3024f867be8af0e1217007e394a8c78f8c0ed41d
HansMbua <hansmbua@yahoo.com>
2021-09-27 18:18:46 +0200

    new commenting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.PrintStream;
 import java.sql.ClientInfoStatus;
 import java.util.*;
 import java.util.function.Function;
 import java.util.function.IntBinaryOperator;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -34,51 +37,61 @@ public class Main {
         System.out.println();
 
         Integer[] values1 = {23, 3, 4, 5, 7, 8, 4, 33, 23};
+        //to print the values
+        //filter them
+        //predicate
+        Predicate<Integer>  predicate = (x -> x % 2 == 0);
+        // sort them
 
-        System.out.printf("original values: %s\n", Arrays.asList(values1));
-        System.out.println();
+        //print them
+        //(parameterList -> {statements})
 
-//        sorting the array
-        System.out.printf("sorted Arrays %s\n", Arrays.stream(values1).sorted().collect(Collectors.toList()));
-        System.out.println();
+        //create arrayList
+        ArrayList arrayList = new ArrayList<String>();
+        //add items in the list
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(23);
+        arrayList.add(232);
+        arrayList.add(40);
+        //print list parameters using lamda
+         arrayList.forEach((n) ->{
+             System.out.println(n);
+         });
 
-//        get values greater than 4
-        List<Integer> greaterThan4 = Arrays.stream(values1).filter(number -> number > 4).collect(Collectors.toList());
-        System.out.printf("get values greater then 4 :%s\n", greaterThan4);
-        System.out.println();
+//        System.out.printf("original values: %s\n", Arrays.asList(values1));
+//        System.out.println();
+//
+////        sorting the array
+//        System.out.printf("sorted Arrays %s\n", Arrays.stream(values1).sorted().collect(Collectors.toList()));
+//        System.out.println();
+//
+////        get values greater than 4
+//        List<Integer> greaterThan4 = Arrays.stream(values1).filter(number -> number > 4).collect(Collectors.toList());
+//        System.out.printf("get values greater then 4 :%s\n", greaterThan4);
+//        System.out.println();
+//
+//        //sorted numbers grearter then 4
+//        List<Integer> sortGreaterthan4 = Arrays.stream(values1).filter(number -> number > 4).sorted().collect(Collectors.toList());
+//        System.out.printf("sorted numbes greater then 4 : %s\n", sortGreaterthan4);
+//        System.out.println();
+//
+//        //generating list sorted with stream
+//        List<Integer> sorttedStream = Arrays.stream(values1).sorted().collect(Collectors.toList());
+//        System.out.printf("list sorted with streams :%s\n ", sorttedStream);
+//        System.out.println();
+//
+//
+////       testing Objects with Streams
+//        System.out.println("testing object with streams");
+//        List<Student> studentList = new ArrayList<>();
+//        Student[] studentCLass = {
+//                new Student("james", "hans", 38),
+//                new Student("Hans", "hans", 32),
+//                new Student("Lovert", "hans", 1),
+//                new Student("Jeo", "Valandi", 45)
+//        };
 
-        //sorted numbers grearter then 4
-        List<Integer> sortGreaterthan4 = Arrays.stream(values1).filter(number -> number > 4).sorted().collect(Collectors.toList());
-        System.out.printf("sorted numbes greater then 4 : %s\n", sortGreaterthan4);
-        System.out.println();
-
-        //generating list sorted with stream
-        List<Integer> sorttedStream = Arrays.stream(values1).sorted().collect(Collectors.toList());
-        System.out.printf("list sorted with streams :%s\n ", sorttedStream);
-        System.out.println();
-
-
-//       testing Objects with Streams
-        System.out.println("testing object with streams");
-        List<Student> studentList = new ArrayList<>();
-        Student[] studentCLass = {
-                new Student("james", "hans", 38),
-                new Student("Hans", "hans", 32),
-                new Student("Lovert", "hans", 1),
-                new Student("Jeo", "Valandi", 45)
-        };
-
-       Function<Student, String > byFirstName = Student::getFirstName;
-        Function<Student, String > byLastName = Student::getLastName;
-       studentList = Arrays.asList(studentCLass);
-        System.out.println("complete emplyee List");
-        studentList.stream().forEach(System.out::println);
-        Predicate<Student> studentPredicate = (student -> student.getSalary() >= 1);
-        System.out.println("<---- filtter according to age ----->");
-        studentList.stream().filter(studentPredicate).forEach(System.out::println);
-
-        //geting all student firstName
-        studentList.stream().map(Student::getFirstName).distinct().forEach(System.out::println);
 
 
     }
